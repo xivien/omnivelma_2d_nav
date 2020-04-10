d3be7d3f46fc1a7464612d18842a658efc5fa77b
xivien <przemyslaw.szostak95@gmail.com>
2020-04-10 11:47:41 +0200

    working EKF with IMU

--- src/gmapping_pose_broadcaster.cpp
+++ src/gmapping_pose_broadcaster.cpp
@@ -1,53 +1,54 @@
-#include "geometry_msgs/PoseWithCovarianceStamped.h"
-#include "ros/ros.h"
+#include <geometry_msgs/PoseWithCovarianceStamped.h>
+#include <nav_msgs/Odometry.h>
+#include <ros/ros.h>
+#include <tf/transform_listener.h>
 
-#include "message_filters/subscriber.h"
-#include "tf2_geometry_msgs/tf2_geometry_msgs.h"
-#include "tf2_ros/message_filter.h"
-#include "tf2_ros/transform_listener.h"
-
-class PoseDrawer
+class PosePub
 {
 public:
-  PoseDrawer() : tf2_(buffer_), target_frame_("map"), tf2_filter_(point_sub_, buffer_, target_frame_, 10, 0)
+  PosePub()
   {
-    point_sub_.subscribe(n_, "/odom/laser_scan_matcher", 10);
     pub = n_.advertise<geometry_msgs::PoseWithCovarianceStamped>("/gmapping/pose", 10);
-    tf2_filter_.registerCallback(boost::bind(&PoseDrawer::msgCallback, this, _1));
+    sub = n_.subscribe("/odom/noisy", 1000, &PosePub::msgCallback, this);
   }
 
-  //  Callback to register with tf2_ros::MessageFilter to be called when transforms are available
-  void msgCallback(const geometry_msgs::PoseWithCovarianceStampedConstPtr& point_ptr)
+  void msgCallback(const nav_msgs::OdometryConstPtr& point_ptr)
   {
-    geometry_msgs::PoseWithCovarianceStamped point_out;
+    tf::StampedTransform transform;
     try
     {
-      buffer_.transform(*point_ptr, point_out, target_frame_);
-      pub.publish(point_out);
-      // ROS_INFO("pose in map frame(x:%f y:%f z:%f)\n", point_out.pose.pose.position.x, point_out.pose.pose.position.y,
-      //          point_out.pose.pose.position.z);
+      listener.lookupTransform("/map", "/odom", ros::Time(0), transform);
     }
-    catch (tf2::TransformException& ex)
+    catch (tf::TransformException ex)
     {
-      ROS_WARN("Failure %s\n", ex.what());  // Print exception which was caught
+      ROS_ERROR("%s", ex.what());
+      ros::Duration(1.0).sleep();
     }
+
+    geometry_msgs::PoseWithCovarianceStamped g_pose;
+    g_pose.header.frame_id = "map";
+    g_pose.header.stamp = point_ptr->header.stamp;
+    g_pose.header.seq = point_ptr->header.seq;
+
+    g_pose.pose.covariance = point_ptr->pose.covariance;
+    g_pose.pose.pose.orientation = point_ptr->pose.pose.orientation;
+    g_pose.pose.pose.position.x = point_ptr->pose.pose.position.x + transform.getOrigin().x();
+    g_pose.pose.pose.position.y = point_ptr->pose.pose.position.y + transform.getOrigin().y();
+    g_pose.pose.pose.position.z = point_ptr->pose.pose.position.z + transform.getOrigin().z();
+    pub.publish(g_pose);
   }
 
 private:
-  std::string target_frame_;
-  tf2_ros::Buffer buffer_;
-  tf2_ros::TransformListener tf2_;
   ros::NodeHandle n_;
-  message_filters::Subscriber<geometry_msgs::PoseWithCovarianceStamped> point_sub_;
-  tf2_ros::MessageFilter<geometry_msgs::PoseWithCovarianceStamped> tf2_filter_;
-
+  ros::Subscriber sub;
   ros::Publisher pub;
+  tf::TransformListener listener;
 };
 
 int main(int argc, char** argv)
 {
   ros::init(argc, argv, "g_pose");  // Init ROS
-  PoseDrawer gp;                    // Construct class
+  PosePub gp;                       // Construct class
   ros::spin();                      // Run until interupted
   return 0;
 };
