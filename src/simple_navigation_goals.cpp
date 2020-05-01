df561d2a4f079f7b65e34996bc28b74ac687f109
xivien <przemyslaw.szostak95@gmail.com>
2020-05-01 15:44:40 +0200

    working nav program

--- src/simple_navigation_goals.cpp
+++ src/simple_navigation_goals.cpp
@@ -1,41 +1,65 @@
 #include <actionlib/client/simple_action_client.h>
 #include <move_base_msgs/MoveBaseAction.h>
-#include <nav_msgs/Odometry.h>
 #include <ros/ros.h>
-#include "geometry_msgs/PoseStamped.h"
-#include "geometry_msgs/PoseWithCovarianceStamped.h"
-#include "geometry_msgs/Twist.h"
-#include "nav_msgs/Path.h"
-#include "std_msgs/Float64.h"
-#include "std_srvs/Empty.h"
+#include <geometry_msgs/PoseWithCovarianceStamped.h>
+#include <geometry_msgs/Twist.h>
+#include <std_srvs/Empty.h>
+
 typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
 
 class MoveBaseController
 {
 public:
-  MoveBaseController(int i) : ac_("move_base", true), mode_(i)
+  MoveBaseController() : ac_("move_base", true)
   {
-    goal_x_ = 1;
-    goal_y_ = -1;
-
+    pub_init_pose_ = n_.advertise<geometry_msgs::PoseWithCovarianceStamped>("/initialpose", 10);
     pub = n_.advertise<geometry_msgs::Twist>("/cmd_vel", 10);
     sub = n_.subscribe("/amcl_pose", 1, &MoveBaseController::msgCallback, this);
+    ROS_INFO("MAKE SURE THAT ROBOT HAS AN EMPTY 1mX1m AREA IN FRONT OF IT");
+    ROS_INFO("Select mode: \n"
+             "0 - reset localization and do homing sequence \n"
+             "1 - set manual localization and do homing sequence \n"
+             "2 - no homing sequence start with localization 0.0 \n");
+    ros::Duration(1.0).sleep();
+    std::cin >> mode_;
 
     freq_ = 10.0f;
     ros::Rate loop_rate(freq_);
-
-    if (mode_ != 0)
+    // reset AMCL particles
+    if (mode_ == 0)
     {
       ROS_INFO("Resetting AMCL POSE");
       g_l_client_ = n_.serviceClient<std_srvs::Empty>("/global_localization");
-
-      // reset AMCL particles
       std_srvs::Empty emptySrv;
       g_l_client_.call(emptySrv);
       ros::Duration(1.0).sleep();
+    }
+    // Set new starting position
+    else if (mode_ == 1)
+    {
+      geometry_msgs::PoseWithCovarianceStamped init_pose;
+      ros::Duration(1.0).sleep();
+      init_pose.header.frame_id = "map";
+      init_pose.pose.pose.orientation.w = 1;
+      ROS_INFO("Enter X[m] Y[m] Theta[rad]");
+      ros::Duration(1.0).sleep();
+      std::cin >> init_pose.pose.pose.position.x >> init_pose.pose.pose.position.y >> init_pose.pose.pose.orientation.z;
 
+      // init covariances
+      init_pose.pose.covariance[0] = 0.25;
+      init_pose.pose.covariance[7] = 0.25;
+      init_pose.pose.covariance[14] = 0.0625;
+      ROS_INFO("Setting pose to x=%.2f y=%.2f theta=%.2f", init_pose.pose.pose.position.x,
+               init_pose.pose.pose.position.y, init_pose.pose.pose.orientation.z);
+      ros::Duration(1.0).sleep();
+      pub_init_pose_.publish(init_pose);
+      ros::spinOnce();
+    }
+    // do homing sequence
+    if (mode_ < 2)
+    {
       float vel = 0.2f;
-      float omega = 0.2f;
+      float omega = 0.25f;
       float dist = 1.0f;
 
       int mov_steps = freq_ * dist / vel;
@@ -59,7 +83,7 @@ public:
       loop_rate.sleep();
 
       ros::spinOnce();
-      if (cov_x_ > 0.1 || cov_y_ > 0.1)
+      if (cov_x_ > cov_tol_ || cov_y_ > cov_tol_)
       {
         ROS_INFO("High covariance, doing a square 1x1m trajectory");
         for (int i = 0; i < 4; i++)
@@ -80,17 +104,32 @@ public:
       }
     }
 
+    // check quality of localization
     ros::spinOnce();
-    if (cov_x_ > 0.1 || cov_y_ > 0.1)
+    if (cov_x_ > cov_tol_ || cov_y_ > cov_tol_)
     {
-      ROS_WARN("High covariance \n The robot may crash");
+      ROS_WARN("High covariance \n The robot may crash \n"
+               "Aborting navigation");
       ros::Duration(10.0).sleep();
     }
+    // Send navigation goal
     else
     {
-      ros::Duration(1.0).sleep();
-      ROS_INFO("Navigating to a goal");
-      send_goal();
+      while (true)
+      {
+        ROS_INFO("To exit use invalid input");
+        ROS_INFO("Send navigation goal X[m] Y[m] Theta[rad]");
+        ros::Duration(1.0).sleep();
+        std::cin >> goal_x_ >> goal_y_ >> goal_theta_;
+        if (!std::cin)
+        {
+          ROS_INFO("Exiting \n Press ctrl+c to quit the program");
+          break;
+        }
+
+        ROS_INFO("Navigating to a goal x=%.2f y=%.2f theta=%.2f", goal_x_, goal_y_, goal_theta_);
+        send_goal();
+      }
     }
   }
   void send_goal()
@@ -104,6 +143,7 @@ public:
 
     goal_.target_pose.pose.position.x = goal_x_;
     goal_.target_pose.pose.position.y = goal_y_;
+    goal_.target_pose.pose.orientation.z = goal_theta_;
     goal_.target_pose.pose.orientation.w = 1.0;
 
     ROS_INFO("Sending goal");
@@ -125,20 +165,22 @@ public:
 
 private:
   ros::NodeHandle n_;
-  ros::Publisher pub;
+  ros::Publisher pub, pub_init_pose_;
   ros::Subscriber sub;
   MoveBaseClient ac_;
   move_base_msgs::MoveBaseGoal goal_;
   ros::ServiceClient g_l_client_;
+
   float freq_;
-  int mode_, goal_x_, goal_y_;
+  int mode_;
+  float goal_x_, goal_y_, goal_theta_;
   double cov_x_, cov_y_;
+  float cov_tol_{ 0.1 };
 };
 int main(int argc, char** argv)
 {
-  int mode = 1;
   ros::init(argc, argv, "MoveBaseController");  // Init ROS
-  MoveBaseController MoveBaseCon(mode);         // Construct class
+  MoveBaseController MoveBaseCon;               // Construct class
   ros::spin();                                  // Run until interupted
   return 0;
 }
