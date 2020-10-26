efd95dfb858a35d3d8e67cc37cc87f3aa5527b45
xivien <przemyslaw.szostak95@gmail.com>
2020-10-26 16:40:44 +0100

    changes

--- init.sh
+++ init.sh
@@ -2,5 +2,6 @@
 
 source ~/$(pwd | awk -F/ '{print FS $4}')/devel/setup.bash
 export GAZEBO_MODEL_PATH=$(pwd)/../omnivelma/src:$GAZEBO_MODEL_PATH
+export GAZEBO_MODEL_PATH=$(pwd)/../models:$GAZEBO_MODEL_PATH
 export GAZEBO_PLUGIN_PATH=$(pwd)/../omnivelma/src:$GAZEBO_PLUGIN_PATH
 export GAZEBO_RESOURCE_PATH=$(pwd)/../omnivelma/src:$GAZEBO_RESOURCE_PATH
